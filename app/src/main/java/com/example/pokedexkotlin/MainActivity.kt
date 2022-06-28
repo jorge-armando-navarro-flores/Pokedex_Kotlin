155cad929d41f42f251f84a6a3c79e8698b9ae62
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-06-28 14:18:02 -0500

    fragment navigation

--- app/src/main/java/com/example/pokedexkotlin/MainActivity.kt
+++ app/src/main/java/com/example/pokedexkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.pokedexkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity(), ListFragment.PokemonSelectListener {
     private lateinit var detailFragment: DetailFragment
@@ -10,10 +11,10 @@ class MainActivity : AppCompatActivity(), ListFragment.PokemonSelectListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        detailFragment = supportFragmentManager.findFragmentById(R.id.detail_fragment) as DetailFragment
+
     }
 
     override fun onPokemonSelected(pokemon: Pokemon) {
-        detailFragment.setPokemonData(pokemon)
+        findNavController(R.id.main_navigation_container).navigate(ListFragmentDirections.actionListFragmentToPokemonDetailFragment(pokemon))
     }
 }
--- app/src/main/java/com/example/pokedexkotlin/Pokemon.kt
+++ app/src/main/java/com/example/pokedexkotlin/Pokemon.kt
@@ -1,6 +1,12 @@
 package com.example.pokedexkotlin
 
-data class Pokemon(val id: Long, val name: String, val hp: Int, val attack: Int, val defense: Int, val speed: Int, val type: PokemonType, val imageUrl: String, val soundId: Int) {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class Pokemon(val id: Long, val name: String, val hp: Int, val attack: Int, val defense: Int, val speed: Int, val type: PokemonType, val imageUrl: String, val soundId: Int) :
+    Parcelable {
 
     enum class PokemonType {
         GRASS, FIRE, WATER, FIGHTER, ELECTRIC
