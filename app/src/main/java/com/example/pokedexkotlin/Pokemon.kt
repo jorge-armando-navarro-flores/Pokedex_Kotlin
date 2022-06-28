8ee1b030175cd9e9dfe431c8b3fe2639fca52165
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-06-27 20:09:36 -0500

    images and sounds

--- app/src/main/java/com/example/pokedexkotlin/Pokemon.kt
+++ app/src/main/java/com/example/pokedexkotlin/Pokemon.kt
@@ -1,6 +1,6 @@
 package com.example.pokedexkotlin
 
-data class Pokemon(val id: Long, val name: String, val hp: Int, val attack: Int, val defense: Int, val speed: Int, val type: PokemonType) {
+data class Pokemon(val id: Long, val name: String, val hp: Int, val attack: Int, val defense: Int, val speed: Int, val type: PokemonType, val imageUrl: String, val soundId: Int) {
 
     enum class PokemonType {
         GRASS, FIRE, WATER, FIGHTER, ELECTRIC
